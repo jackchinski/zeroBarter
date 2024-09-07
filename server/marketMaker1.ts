f266223e980e5f90a44267eb3bcf9d623d862d4b
jackchinski <marcin.jacz@gmail.com>
2024-09-07 13:01:31 +0200

    greedy and cheap mm

---
+++ server/marketMaker1.ts
@@ -0,0 +1,33 @@
+import { io as Client } from "socket.io-client";
+
+const socket = Client("http://localhost:3000");
+
+// CHEAP MM 
+
+// say that this is a MM
+socket.emit("isMarketMaker");
+
+// simulate generating a quote with a a fee
+
+function generateQuote(tradeRequest) {
+  const feeAmount = parseFloat((Math.random() * (0.0005 - 0.0001) + 0.0001).toFixed(4)); // random fee
+  return {
+    ...tradeRequest,
+    mmAddress: "0x3814f9F424874860ffCD9f70f0D4B74b81e791E8",
+    marketmakerId: socket.id,
+    fee: feeAmount,
+    assetAmountOut: parseFloat((tradeRequest.assetAmountOut - feeAmount).toFixed(18)),
+  };
+}
+
+socket.on("tradeRequest", (tradeRequest) => {
+  console.log("Received trade request: ", tradeRequest);
+
+  // generate a quote and send it to the server after a small delay
+
+  setTimeout(() => {
+    const quote = generateQuote(tradeRequest);
+    console.log("Sending quote: ", quote);
+    socket.emit("tradeQuote", quote);
+  }, 3000);
+});
--- server/recipient.ts
+++ server/recipient.ts
@@ -7,9 +7,8 @@ let orderId = 1;
 const tradeRequest = {
   orderId: orderId,
   recipientAddress: "0x2F02dE1E2F3010E3BDBe2B05bc07701C8044e801",
-  mmAddress: "",
   assetTypeIn: "SepoliaETH", // can be any arbitrary token or even digital asset
-  amountIn: 0.001,
+  assetAmountIn: 0.001,
   assetTypeOut: "SepoliaETH",
   assetAmountOut: 0.001,
 };
